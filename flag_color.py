81b30419bcf94927defd19a939661b81fd2daa45
DaphneGroot <daphnegroot1996@hotmail.com>
2015-02-20 14:42:32 +0100

    Genereert willkeurige kleuren KLAAR

---
+++ flag_color.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+#country.py
+#Daphne Groot
+#Hennie Veldthuis
+
+from PyQt4 import QtGui
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+import sys
+from random import *
+
+class Flagcolor(QtGui.QColor):
+    def __init__(self):
+        super(Flagcolor, self).__init__()
+        self.initUI()
+        
+        
+    def initUI(self):
+        """ Maakt een combinatie van willkeurige RGB waarden"""
+
+        self.kleur = QtGui.QColor(0, 0, 0)
+        
+        self.kleur.setRed(randrange(0, 256))
+        self.kleur.setGreen(randrange(0, 256))
+        self.kleur.setBlue(randrange(0, 256))
+        
+        print(self.kleur)
+        
+        
+def main():
+    app = QtGui.QApplication(sys.argv)
+    Landennaam = Flagcolor()
+    sys.exit(app.exec_())
+
+    
+    
+if __name__ == '__main__':
+    main()
+
+
+
